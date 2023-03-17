4ec170c78e287e9847c1bd7a6103ac40b8a58483
iamteam <robotics.za@gmail.com>
2023-03-18 02:04:17 +0700

    Setup font and system design (#1)
    * feat: setup font and system design
    
    * fix: remove .vscode

---
+++ web/tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "theme-red": "#F07878",
+        "theme-blue": "#78BEF0",
+        "theme-btn-text": "#686868",
+        "theme-btn": "#D9D9D9",
+      },
+      fontSize: {
+        "btn": [
+          "36px",
+          {
+            fontWeight: "400"
+          }
+        ],
+        "win-score": [
+          "48px",
+          {
+            fontWeight: "400"
+          }
+        ],
+        "lose-score": [
+          "36px",
+          {
+            fontWeight: "400"
+          }
+        ],
+        "big-score": [
+          "75px",
+          {
+            fontWeight: "semibold"
+          }
+        ],
+        "u-name": [
+          "20px",
+          {
+            fontWeight: "bold"
+          }
+        ],
+        "h1": [
+          "64px",
+          {
+            fontWeight: "400"
+          }
+        ]
+
+      }
+    },
+  },
+  plugins: [],
+}
