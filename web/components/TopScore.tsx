c0d9a0b44bd6d76debfacfc1b08a31a35eccb45d
vixtory09678 <robotics.za@gmail.com>
2023-03-28 02:06:10 +0700

    feat: add top score component to show high rank

---
+++ web/components/TopScore.tsx
@@ -0,0 +1,23 @@
+export function TopScore() {
+  return (
+    <>
+      <div className="top-score">
+        <div className="top-2nd">
+          <p className="text-h1 opacity-60">2</p>
+          <p className="text-big-score -translate-y-5">236</p>
+          <p className="text-u-name -translate-y-7">MyName</p>
+        </div>
+        <div className="top-1st">
+          <p className="text-h1 opacity-60">1</p>
+          <p className="text-big-score">523</p>
+          <p className="text-u-name">TeamTeam</p>
+        </div>
+        <div className="top-3rd">
+          <p className="text-h1 opacity-60">3</p>
+          <p className="text-big-score -translate-y-5">32</p>
+          <p className="text-u-name -translate-y-7">GuBoy</p>
+        </div>
+      </div>
+    </>
+  );
+}
